acf1daa1430706c17834aa538979d0575d3b6210
shania-d <shania.dalal00@gmail.com>
2021-03-08 17:43:42 +0530

    Diseases Mental Tips services added

--- healthreminder/src/main/java/com/cg/healthreminder/services/DiseaseServices.java
+++ healthreminder/src/main/java/com/cg/healthreminder/services/DiseaseServices.java
@@ -4,4 +4,7 @@ import com.cg.healthreminder.model.Diseases;
 
 public interface DiseaseServices {
 	public Diseases viewDisease(int dis_id);
+	public Diseases updateDiseaseInfo(int dis_id, String content);
+	public Diseases addDisease(Diseases d);
+	public boolean deleteDisease(int dis_id);
 }
--- healthreminder/src/main/java/com/cg/healthreminder/services/MentalHealthServices.java
+++ healthreminder/src/main/java/com/cg/healthreminder/services/MentalHealthServices.java
@@ -1,5 +1,10 @@
 package com.cg.healthreminder.services;
 
+import com.cg.healthreminder.model.mentalHealth;
+
 public interface MentalHealthServices {
-	public String displayTips(int mental_rating);
+	public mentalHealth displayTips(int mental_rating);
+	public mentalHealth updateTips(int mental_rating, String uptips);
+	public mentalHealth addTips(mentalHealth m);
+	public boolean deleteTips(int mental_rating);
 }
