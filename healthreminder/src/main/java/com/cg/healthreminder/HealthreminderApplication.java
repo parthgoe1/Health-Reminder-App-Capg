3697104f7dabe087ce0a6fe97cd034efbc88c017
Parth_Goel <goel.parth210@gmail.com>
2021-03-08 12:22:58 +0530

    Project commit

---
+++ healthreminder/src/main/java/com/cg/healthreminder/HealthreminderApplication.java
@@ -0,0 +1,13 @@
+package com.cg.healthreminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthreminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthreminderApplication.class, args);
+	}
+
+}
