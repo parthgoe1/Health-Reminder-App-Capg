383f4d90686fe13a33edd3e178fbbc005ed4bf97
Parth_Goel <goel.parth210@gmail.com>
2021-03-09 13:17:22 +0530

    more more test cases added

--- healthreminder/src/test/java/com/cg/healthreminder/dao/FollowUpDietStatusInfoJpaDaoTest.java
+++ healthreminder/src/test/java/com/cg/healthreminder/dao/FollowUpDietStatusInfoJpaDaoTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.cg.healthreminder.model.DietInfo;
 import com.cg.healthreminder.model.FollowUpDietStatusInfo;
 
 import java.util.List;
@@ -41,5 +40,57 @@ public class FollowUpDietStatusInfoJpaDaoTest {
     	FollowUpDietStatusInfo getFromInDb = followUpDietStatusInfoJpaDao.findById(saveInDb.getPatientId()).get();
         assertThat(getFromInDb).isEqualTo(saveInDb);
     }
+    
+    @Test
+    public void testGetFollowUpDietStatusById() throws Exception{
+    	FollowUpDietStatusInfo followUpDietStatusInfo = new FollowUpDietStatusInfo();
+    	followUpDietStatusInfo.setPatientId(2);
+    	followUpDietStatusInfo.setDietStatus(true);
+       
+    	FollowUpDietStatusInfo saveInDb = testEntityManager.persist(followUpDietStatusInfo);
+        
+    	FollowUpDietStatusInfo getInDb = followUpDietStatusInfoJpaDao.findById(followUpDietStatusInfo.getPatientId()).get();
+        assertThat(getInDb).isEqualTo(saveInDb);
+    }
+    
+    @Test
+    public void testDeleteFollowUpDietStatusInfoById() throws Exception{
+    	FollowUpDietStatusInfo followUpDietStatusInfo1 = new FollowUpDietStatusInfo();
+    	followUpDietStatusInfo1.setPatientId(2);
+    	followUpDietStatusInfo1.setDietStatus(true);
+        
+        FollowUpDietStatusInfo followUpDietStatusInfo2 = new FollowUpDietStatusInfo();
+        followUpDietStatusInfo2.setPatientId(3);
+        followUpDietStatusInfo2.setDietStatus(true);
+
+        FollowUpDietStatusInfo followUpDietStatusInfo = testEntityManager.persist(followUpDietStatusInfo1);
+        testEntityManager.persist(followUpDietStatusInfo2);
+
+        //delete one ticket DB
+        testEntityManager.remove(followUpDietStatusInfo);
+
+        List<FollowUpDietStatusInfo> followUpDietStatusInfos = (List<FollowUpDietStatusInfo>) followUpDietStatusInfoJpaDao.findAll();
+        Assert.assertEquals(followUpDietStatusInfos.size(), 1);
+
+    }
+    
+    @Test
+    public void testUpdateFollowUpDietStatusInfo(){
+
+    	FollowUpDietStatusInfo followUpDietStatusInfo = new FollowUpDietStatusInfo();
+    	followUpDietStatusInfo.setPatientId(3);
+    	followUpDietStatusInfo.setDietStatus(false);
+        
+        testEntityManager.persist(followUpDietStatusInfo);
+
+        FollowUpDietStatusInfo getFromDb = followUpDietStatusInfoJpaDao.findById(3).get();
+        getFromDb.setDietStatus(true);
+        testEntityManager.persist(getFromDb);
+
+        assertThat(getFromDb.getDietStatus()).isEqualTo(true);
+    }
+
+
+    
     
 }
