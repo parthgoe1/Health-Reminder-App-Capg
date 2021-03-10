4c9600628d35449f71b360a331c69e9d298ab664
bunty56 <buntysayu9@gmail.com>
2021-03-10 17:15:43 +0530

    logger half-done

--- healthreminder/src/main/java/com/cg/healthreminder/services/impl/AppointmentDetailsServicesImpl.java
+++ healthreminder/src/main/java/com/cg/healthreminder/services/impl/AppointmentDetailsServicesImpl.java
@@ -7,6 +7,8 @@
 
 
 package com.cg.healthreminder.services.impl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.cg.healthreminder.dao.AppointmentDetailsJpaDao;
@@ -21,9 +23,13 @@ import javax.transaction.Transactional;
 public class AppointmentDetailsServicesImpl implements AppointmentDetailsServices {
 	@Autowired
     private AppointmentDetailsJpaDao appointmentDetailsJpaDao;
+	private static final Logger logger=LogManager.getLogger(AppointmentDetailsServicesImpl.class);
 	
 	@Override
 	public AppointmentDetails findAppointmentDetailByPatientId(int patient_id) throws AllCustomException {
+		
+		logger.info("Finding appointment details by patient Id ........");
+		
 		AppointmentDetails a= appointmentDetailsJpaDao.findByPatientId(patient_id);
 		if(a==null) {
 			throw new AllCustomException("Appointment not found for the given Patient id");
@@ -33,6 +39,9 @@ public class AppointmentDetailsServicesImpl implements AppointmentDetailsService
 	
 	
 	public AppointmentDetails findAppointmentDetailByDoctorId(int doc_id) throws AllCustomException {
+		
+		logger.info("Finding appointment details by doctor Id ........");
+		
 		AppointmentDetails a= appointmentDetailsJpaDao.findByDoctorId(doc_id);
 		if(a==null) {
 			throw new AllCustomException("Appointment not found for the given Doctor id");
@@ -41,10 +50,14 @@ public class AppointmentDetailsServicesImpl implements AppointmentDetailsService
 	}
     
 	public AppointmentDetails createAppointment(AppointmentDetails amp){
-    	return appointmentDetailsJpaDao.save(amp);
+		logger.info("Creating Appointment ........");
+		
+		return appointmentDetailsJpaDao.save(amp);
     }
     
 	public AppointmentDetails deleteAppointmentByPatientId(int patient_id)  throws AllCustomException{
+		
+		logger.info("Deleting appointment details by patient Id ........");
 		AppointmentDetails a= appointmentDetailsJpaDao.findByPatientId(patient_id);
 		if(a!=null) {
 			
@@ -58,6 +71,8 @@ public class AppointmentDetailsServicesImpl implements AppointmentDetailsService
 	
 	
     public AppointmentDetails deleteAppointmentByDoctorId(int doc_id) throws AllCustomException {
+    	logger.info("Deleting appointment details by doctor Id ........");
+    	
     	AppointmentDetails a= appointmentDetailsJpaDao.findByDoctorId(doc_id);
 		if(a!=null) {
 			
