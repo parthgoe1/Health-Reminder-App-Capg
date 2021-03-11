d587f501f8870cbe15106c8d83ac0f78c5478f42
bunty56 <buntysayu9@gmail.com>
2021-03-11 12:37:53 +0530

    test suite implemented

---
+++ healthreminder/src/test/java/com/cg/healthreminder/testsuites/TestSuiteRunner.java
@@ -0,0 +1,16 @@
+package com.cg.healthreminder.testsuites;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class TestSuiteRunner {
+	public static void main(String[] args) {
+	      
+		  Result result = JUnitCore.runClasses(TestSuiteCasesToRun.class);
+	      for (Failure failure : result.getFailures()) {
+	         System.out.println(failure.toString());
+	      }
+			
+	      System.out.println(result.wasSuccessful()+" All test Suites Running");
+	   }
+}
